b730fcad95ce6d4c90a11cee1d2e228f0fc41b3b
Muhammed Shendi <muhammedshendi@gmail.com>
2022-04-10 23:36:06 +0200

    test included

---
+++ store/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from store.models import Category, Product
+
+
+class TestCategoriesModel(TestCase):
+    def setUp(self) -> None:
+        self.data1 = Category.objects.create(name="django", slug="django")
+
+    def test_category_model_entry(self):
+        data = self.data1
+        self.assertIsInstance(data, Category)
+
+    def test_category_model_return(self):
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductsModel(TestCase):
+    def setUp(self) -> None:
+        Category.objects.create(name="django", slug="django")
+        User.objects.create(username="admin")
+        self.data1 = Product.objects.create(
+            category_id=1, title="django beginner", slug="django-beginner", price="20.00", created_by_id=1, image="django")
+
+    def test_product_model_entry(self):
+        data = self.data1
+        self.assertIsInstance(data, Product)
+
+    def test_product_model_return(self):
+        data = self.data1
+        self.assertEqual(str(data), "django beginner")
