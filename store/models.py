a4d86ad794fc9657f16c34d3ec46351cff44bc2b
Muhammed Shendi <muhammedshendi@gmail.com>
2022-04-12 07:32:31 +0200

    templates

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 
 
@@ -9,6 +10,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "categories"
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
 
     def __str__(self) -> str:
         return self.name
@@ -36,8 +40,8 @@ class Product(models.Model):
         verbose_name_plural = 'Products'
         ordering = ('-created',)
 
-    # def get_absolute_url(self):
-    #     return reverse('store:product_detail', args=[self.slug])
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
