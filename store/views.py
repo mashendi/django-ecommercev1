5577368f11b5a1441b8c823e86b561687166c0b3
Muhammed Shendi <muhammedshendi@gmail.com>
2022-04-12 10:15:17 +0200

    refactor naming

--- store/views.py
+++ store/views.py
@@ -5,17 +5,17 @@ from .models import Category, Product
 # Create your views here.
 
 
-def all_products(request):
+def product_all(request):
     products = Product.products.all()
     return render(request, 'store/home.html', {'products': products})
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, "store/products/detail.html", {'product': product})
+    return render(request, "store/products/single.html", {'product': product})
 
 
-def category_list(request, category_slug):
+def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
     return render(request, 'store/products/category.html', {'category': category, 'products': products})
