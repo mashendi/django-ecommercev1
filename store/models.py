0182bf0069a022877965c3147390b1c25cce0ece
Muhammed Shendi <muhammedshendi@gmail.com>
2022-04-12 10:22:20 +0200

    add default image for product

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
@@ -30,7 +31,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default='images/default.jpg')
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     in_stock = models.BooleanField(default=True)
