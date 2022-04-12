c31c2e55d6798df7a91b7095ecc56374205ac185
Muhammed Shendi <muhammedshendi@gmail.com>
2022-04-12 08:27:40 +0200

    add test views

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
 from django.contrib.auth.models import User
+from django.test import TestCase
+
 from store.models import Category, Product
 
 
@@ -21,7 +22,8 @@ class TestProductsModel(TestCase):
         Category.objects.create(name="django", slug="django")
         User.objects.create(username="admin")
         self.data1 = Product.objects.create(
-            category_id=1, title="django beginner", slug="django-beginner", price="20.00", created_by_id=1, image="django")
+            category_id=1, title="django beginner", slug="django-beginner",
+            price="20.00", created_by_id=1, image="django")
 
     def test_product_model_entry(self):
         data = self.data1
---
+++ store/tests/test_views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from store.models import Category, Product
+from store.views import all_products
+
+
+class TestViewResponses(TestCase):
+    def setUp(self) -> None:
+        self.c = Client()
+        Category.objects.create(name="django", slug="django")
+        User.objects.create(username="admin")
+        self.data1 = Product.objects.create(
+            category_id=1, title="django beginner", slug="django-beginner",
+            price="20.00", created_by_id=1, image="django")
+
+    def test_url_allowed_hosts(self):
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_product_detail_url(self):
+        response = self.c.get(
+            reverse('store:product_detail', args=['django-beginner']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_category_list_url(self):
+        response = self.c.get(reverse('store:category_list', args=['django']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_html(self):
+        request = HttpRequest()
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
